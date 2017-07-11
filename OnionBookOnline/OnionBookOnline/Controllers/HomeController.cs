dd7dc7e58e051589c7b9568867f366af99460116
zjzsliyang <zjzsliyang@gmail.com>
2017-07-11 11:22:28 +0800

    feat(view): add contact

--- OnionBookOnline/OnionBookOnline/Controllers/HomeController.cs
+++ OnionBookOnline/OnionBookOnline/Controllers/HomeController.cs
@@ -22,8 +22,6 @@ namespace OnionBookOnline.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
