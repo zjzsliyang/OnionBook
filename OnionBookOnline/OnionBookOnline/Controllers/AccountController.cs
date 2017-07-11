f3dd3e48abd62bad338d4215dfcf8e821cde2bc9
zjzsliyang <zjzsliyang@gmail.com>
2017-07-11 14:42:35 +0800

    feat(view): add cart view

--- OnionBookOnline/OnionBookOnline/Controllers/AccountController.cs
+++ OnionBookOnline/OnionBookOnline/Controllers/AccountController.cs
@@ -54,7 +54,7 @@ namespace OnionBookOnline.Controllers
 
         public ActionResult Orderlist()
         {
-            return View;
+            return View();
         }
 
         public ActionResult Cart()
