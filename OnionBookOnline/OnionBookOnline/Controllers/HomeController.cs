633dc89d6a20bb4c02a35b3025bc2ff8ffd5b08c
zjzsliyang <zjzsliyang@gmail.com>
2017-07-13 08:27:36 +0800

    fix(all): fix bugs & stable version

--- OnionBookOnline/OnionBookOnline/Controllers/HomeController.cs
+++ OnionBookOnline/OnionBookOnline/Controllers/HomeController.cs
@@ -3,19 +3,133 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OnionBookOnline.DAL;
 using OnionBookOnline.Models;
 
 namespace OnionBookOnline.Controllers
 {
     public class HomeController : Controller
     {
+        private IEnumerable<HomeBookInfo> queryOnSales(OnionContext context, string primaryid)
+        {
+            IEnumerable<HomeBookInfo> qsp = from b in context.books
+                                            join c in context.pictures on b.BOOKID equals c.BOOKID
+                                            join d in context.writes on b.BOOKID equals d.BOOKID
+                                            join e in context.authors on d.AUTHORID equals e.AUTHORID
+                                            where b.PRIMARYID == primaryid
+                                            orderby b.DISCOUNT ascending
+                                            select new HomeBookInfo()
+                                            {
+                                                NAME = b.NAME,
+                                                AUTHORNAME = e.NAME,
+                                                SCORE = b.SCORE,
+                                                PRICE = b.PRICE,
+                                                DISCOUNT = b.DISCOUNT,
+                                                PATH = c.PATH
+                                            };
+            return qsp;
+        }
         public ActionResult Index()
         {
-            ViewBag.count = "1";
-            ViewBag.prize = "244元";
-            ViewBag.item = new string[] { "假装是一本书", "数量：1", "244元" };
+            var HomeVM = new HomeIndexViewModel();
+            HomeVM.bpsLit = new List<HomeBookInfo>();
+            HomeVM.bpsPop = new List<HomeBookInfo>();
+            HomeVM.bpsCul = new List<HomeBookInfo>();
+            HomeVM.bpsLif = new List<HomeBookInfo>();
+            HomeVM.bpsEco = new List<HomeBookInfo>();
+            HomeVM.bpsTec = new List<HomeBookInfo>();
+            HomeVM.bpsRank = new List<HomeBookInfo>();
+            HomeVM.bpsScore = new List<HomeBookInfo>();
+            HomeVM.bpsLatest = new List<HomeBookInfo>();
 
-            return View();
+            using (var context = new OnionContext())
+            {
+
+                //On sales
+                var queryLit = queryOnSales(context, "01");
+                var queryPop = queryOnSales(context, "02");
+                var queryCul = queryOnSales(context, "03");
+                var queryLif = queryOnSales(context, "04");
+                var queryEco = queryOnSales(context, "05");
+                var queryTec = queryOnSales(context, "06");
+
+                var resLit = queryLit.ToList();
+                var resPop = queryPop.ToList();
+                var resCul = queryCul.ToList();
+                var resLif = queryLif.ToList();
+                var resEco = queryEco.ToList();
+                var resTec = queryTec.ToList();
+
+
+                //best seller
+                var queryRank = from b in context.books
+                                join c in context.pictures on b.BOOKID equals c.BOOKID
+                                join d in context.writes on b.BOOKID equals d.BOOKID
+                                join e in context.authors on d.AUTHORID equals e.AUTHORID
+                                orderby b.SALE descending
+                                select new HomeBookInfo()
+                                {
+                                    NAME = b.NAME,
+                                    AUTHORNAME = e.NAME,
+                                    SCORE = b.SCORE,
+                                    PRICE = b.PRICE,
+                                    DISCOUNT = b.DISCOUNT,
+                                    PATH = c.PATH
+                                };
+                var resRank = queryRank.ToList();
+
+                //Good reputation
+                var queryScore = from b in context.books
+                                 join c in context.pictures on b.BOOKID equals c.BOOKID
+                                 join d in context.writes on b.BOOKID equals d.BOOKID
+                                 join e in context.authors on d.AUTHORID equals e.AUTHORID
+                                 orderby b.SCORE descending
+                                 select new HomeBookInfo()
+                                 {
+                                     NAME = b.NAME,
+                                     AUTHORNAME = e.NAME,
+                                     SCORE = b.SCORE,
+                                     PRICE = b.PRICE,
+                                     DISCOUNT = b.DISCOUNT,
+                                     PATH = c.PATH
+                                 };
+                var resScore = queryScore.ToList();
+
+                //Latest
+                var queryLatest = from b in context.books
+                                  join c in context.pictures on b.BOOKID equals c.BOOKID
+                                  join d in context.writes on b.BOOKID equals d.BOOKID
+                                  join e in context.authors on d.AUTHORID equals e.AUTHORID
+                                  orderby b.PUBLISHINGDATE descending
+                                  select new HomeBookInfo()
+                                  {
+                                      NAME = b.NAME,
+                                      AUTHORNAME = e.NAME,
+                                      SCORE = b.SCORE,
+                                      PRICE = b.PRICE,
+                                      DISCOUNT = b.DISCOUNT,
+                                      PATH = c.PATH
+                                  };
+                var resLatest = queryLatest.ToList();
+
+
+
+                for (int i = 0; i < 10; i++)
+                {
+                    HomeVM.bpsLit.Add(resLit[i]);
+                    HomeVM.bpsPop.Add(resPop[i]);
+                    HomeVM.bpsCul.Add(resCul[i]);
+                    HomeVM.bpsLif.Add(resLif[i]);
+                    HomeVM.bpsEco.Add(resEco[i]);
+                    HomeVM.bpsTec.Add(resTec[i]);
+                    HomeVM.bpsRank.Add(resRank[i]);
+                    HomeVM.bpsScore.Add(resScore[i]);
+                    HomeVM.bpsLatest.Add(resLatest[i]);
+                }
+
+            }
+
+            return View(HomeVM);
         }
 
         public ActionResult Contact()
---
+++ OnionBookOnline/OnionBookOnline/Models/HomeModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OnionBookOnline.Models
+{
+    public class HomeBookInfo
+    {
+
+        public string NAME { get; set; }
+        public string AUTHORNAME { get; set; }
+        public double SCORE { get; set; }
+        public double PRICE { get; set; }
+        public double DISCOUNT { get; set; }
+        public int SALE { get; set; }
+        public string PUBLISHINGDATE { get; set; }
+        public string PATH { get; set; }
+
+        public HomeBookInfo()
+        {
+
+        }
+
+    }
+
+}
