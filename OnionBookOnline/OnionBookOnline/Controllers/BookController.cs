04dfccec2f0366e9b9af163e0dbf46a8984897fe
Frank1129 <18917857061@163.com>
2017-07-13 22:28:21 +0800

    Add files via upload

--- OnionBookOnline/OnionBookOnline/Controllers/BookController.cs
+++ OnionBookOnline/OnionBookOnline/Controllers/BookController.cs
@@ -347,58 +347,63 @@ namespace OnionBookOnline.Controllers
             var bkVM = new BookViewModel();
             using (var context = new OnionContext())
             {
-                string pID=null, sID=null;
+                string pID = null, sID = null, chname = null;
                 typename = typename.Replace("/", "");
                 switch (typename)
                 {
-                    case "fiction": { pID = "01";sID = "01";break; }
-                    case "scatter": { pID = "01"; sID = "02"; break; }
-                    case "prose": { pID = "01"; sID = "03"; break; }
-                    case "poetry": { pID = "01"; sID = "04"; break; }
-                    case "fairytale": { pID = "01"; sID = "05"; break; }
-                    case "essay": { pID = "01"; sID = "06"; break; }
-                    case "child": { pID = "01"; sID = "07"; break; }
-                    case "masterpiece": { pID = "01"; sID = "08"; break; }
-                    case "comic": { pID = "02"; sID = "01"; break; }
-                    case "painting": { pID = "02"; sID = "02"; break; }
-                    case "romance": { pID = "02"; sID = "03"; break; }
-                    case "reasoning": { pID = "02"; sID = "04"; break; }
-                    case "sf": { pID = "02"; sID = "05"; break; }
-                    case "ma": { pID = "02"; sID = "06"; break; }
-                    case "youthfulness": { pID = "02"; sID = "07"; break; }
-                    case "suspense": { pID = "02"; sID = "08"; break; }
-                    case "history": { pID = "03"; sID = "01"; break; }
-                    case "psychological": { pID = "03"; sID = "02"; break; }
-                    case "society": { pID = "03"; sID = "03"; break; }
-                    case "philosophy": { pID = "03"; sID = "04"; break; }
-                    case "art": { pID = "03"; sID = "05"; break; }
-                    case "politics": { pID = "03"; sID = "06"; break; }
-                    case "building": { pID = "03"; sID = "07"; break; }
-                    case "religion": { pID = "03"; sID = "08"; break; }
-                    case "travel": { pID = "04"; sID = "01"; break; }
-                    case "photography": { pID = "04"; sID = "02"; break; }
-                    case "food": { pID = "04"; sID = "03"; break; }
-                    case "health": { pID = "04"; sID = "04"; break; }
-                    case "education": { pID = "04"; sID = "05"; break; }
-                    case "home": { pID = "04"; sID = "06"; break; }
-                    case "workplace": { pID = "04"; sID = "07"; break; }
-                    case "gender": { pID = "04"; sID = "08"; break; }
-                    case "economic": { pID = "05"; sID = "01"; break; }
-                    case "management": { pID = "05"; sID = "02"; break; }
-                    case "financial": { pID = "05"; sID = "03"; break; }
-                    case "investment": { pID = "05"; sID = "04"; break; }
-                    case "marketing": { pID = "05"; sID = "05"; break; }
-                    case "fm": { pID = "05"; sID = "06"; break; }
-                    case "stock": { pID = "05"; sID = "07"; break; }
-                    case "ad": { pID = "05"; sID = "08"; break; }
-                    case "sc": { pID = "06"; sID = "01"; break; }
-                    case "science": { pID = "06"; sID = "02"; break; }
-                    case "interaction": { pID = "06"; sID = "03"; break; }
-                    case "programming": { pID = "06"; sID = "04"; break; }
-                    case "algorithms": { pID = "06"; sID = "05"; break; }
-                    case "communication": { pID = "06"; sID = "06"; break; }
-                    case "program": { pID = "06"; sID = "07"; break; }
-                    case "internet": { pID = "06"; sID = "08"; break; }
+                    case "fiction": { pID = "01";sID = "01";chname = "小说"; break; }
+                    case "scatter": { pID = "01"; sID = "02"; chname = "随笔"; break; }
+                    case "prose": { pID = "01"; sID = "03"; chname = "散文"; break; }
+                    case "poetry": { pID = "01"; sID = "04"; chname = "诗歌"; break; }
+                    case "fairytale": { pID = "01"; sID = "05"; chname = "童话"; break; }
+                    case "essay": { pID = "01"; sID = "06"; chname = "杂文"; break; }
+                    case "child": { pID = "01"; sID = "07"; chname = "儿童"; break; }
+                    case "masterpiece": { pID = "01"; sID = "08"; chname = "名著"; break; }
+
+                    case "comic": { pID = "02"; sID = "01"; chname = "漫画"; break; }
+                    case "painting": { pID = "02"; sID = "02"; chname = "绘本"; break; }
+                    case "romance": { pID = "02"; sID = "03"; chname = "言情"; break; }
+                    case "reasoning": { pID = "02"; sID = "04"; chname = "推理"; break; }
+                    case "sf": { pID = "02"; sID = "05"; chname = "科幻"; break; }
+                    case "ma": { pID = "02"; sID = "06"; chname = "武侠"; break; }
+                    case "youthfulness": { pID = "02"; sID = "07"; chname = "青春"; break; }
+                    case "suspense": { pID = "02"; sID = "08"; chname = "悬疑"; break; }
+
+                    case "history": { pID = "03"; sID = "01"; chname = "历史"; break; }
+                    case "psychological": { pID = "03"; sID = "02"; chname = "心理"; break; }
+                    case "society": { pID = "03"; sID = "03"; chname = "社会"; break; }
+                    case "philosophy": { pID = "03"; sID = "04"; chname = "哲学"; break; }
+                    case "art": { pID = "03"; sID = "05"; chname = "艺术"; break; }
+                    case "politics": { pID = "03"; sID = "06"; chname = "政治"; break; }
+                    case "building": { pID = "03"; sID = "07"; chname = "建筑"; break; }
+                    case "religion": { pID = "03"; sID = "08"; chname = "宗教"; break; }
+
+                    case "travel": { pID = "04"; sID = "01"; chname = "旅行"; break; }
+                    case "photography": { pID = "04"; sID = "02"; chname = "摄影"; break; }
+                    case "food": { pID = "04"; sID = "03"; chname = "美食"; break; }
+                    case "health": { pID = "04"; sID = "04"; chname = "健康"; break; }
+                    case "education": { pID = "04"; sID = "05"; chname = "教育"; break; }
+                    case "home": { pID = "04"; sID = "06"; chname = "家居"; break; }
+                    case "workplace": { pID = "04"; sID = "07"; chname = "职场"; break; }
+                    case "gender": { pID = "04"; sID = "08"; chname = "两性"; break; }
+
+                    case "economic": { pID = "05"; sID = "01"; chname = "经济"; break; }
+                    case "management": { pID = "05"; sID = "02"; chname = "管理"; break; }
+                    case "financial": { pID = "05"; sID = "03"; chname = "金融"; break; }
+                    case "investment": { pID = "05"; sID = "04"; chname = "投资"; break; }
+                    case "marketing": { pID = "05"; sID = "05"; chname = "营销"; break; }
+                    case "fm": { pID = "05"; sID = "06"; chname = "理财"; break; }
+                    case "stock": { pID = "05"; sID = "07"; chname = "股票"; break; }
+                    case "ad": { pID = "05"; sID = "08"; chname = "广告"; break; }
+
+                    case "sc": { pID = "06"; sID = "01"; chname = "科普"; break; }
+                    case "science": { pID = "06"; sID = "02"; chname = "科学"; break; }
+                    case "interaction": { pID = "06"; sID = "03"; chname = "交互"; break; }
+                    case "programming": { pID = "06"; sID = "04"; chname = "编程"; break; }
+                    case "algorithms": { pID = "06"; sID = "05"; chname = "算法"; break; }
+                    case "communication": { pID = "06"; sID = "06"; chname = "通信"; break; }
+                    case "program": { pID = "06"; sID = "07"; chname = "程序"; break; }
+                    case "internet": { pID = "06"; sID = "08"; chname = "互联网"; break; }
                 }
 
 
@@ -427,7 +432,7 @@ namespace OnionBookOnline.Controllers
                                 SECONDARYID = b.SECONDARYID
                             };
                 bkVM.typeBook = new List<Detailbook>(query.ToList());
-                bkVM.type = typename;
+                bkVM.type = chname;
             }
             return View(bkVM);
         }
